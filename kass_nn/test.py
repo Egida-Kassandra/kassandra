17a5f4812126e7dc55b77182f007ff9add06a413
antonioalfa22 <antonioalfa22@gmail.com>
2020-03-17 21:45:37 +0100

    Añadido requirements.txt

--- kass_nn/test.py
+++
@@ -1,8 +0,0 @@
-from kass_nn.parse_logs import LogParser
-
-if __name__ == '__main__':
-    train_data = []
-    train_labels = []
-    parse_logs = LogParser()
-    train_data = parse_logs.parse_file('fool.log', train_data)
-    print(len(train_data))
